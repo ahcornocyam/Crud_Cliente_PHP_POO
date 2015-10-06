8da1961fffa3ae278cec50dedfc5281ee00b8fae
Alexandre Roch <maycon.alex.rocha@hotmail.com>
2015-10-06 16:50:39 -0300

    criando a classe Cliente.php

--- Class/Cliente.php
+++ Class/Cliente.php
@@ -1,15 +1,152 @@
 <?php
-    
 
-    Class Cliente
+/**
+ * Created by PhpStorm.
+ * User: alexroch
+ * Date: 06/10/15
+ * Time: 16:48
+ */
+class Cliente
+{
+    private $nome;
+    private $sobrenome;
+    private $idade;
+    private $cpf;
+    private $cidade;
+    private $email;
+    private $fone;
+
+    /**
+     * Cliente constructor.
+     * @param $nome
+     * @param $sobrenome
+     * @param $idade
+     * @param $cpf
+     * @param $cidade
+     * @param $email
+     * @param $fone
+     */
+    public function __construct($nome, $sobrenome, $idade, $cpf, $cidade, $email, $fone)
     {
-        
-        
-        private $nome;
-        private $sobrenome;
-        private $idade;
-        private $cpf;
-        private $cidade;
-        private $email;
-        private $fone;
+        $this->nome = $nome;
+        $this->sobrenome = $sobrenome;
+        $this->idade = $idade;
+        $this->cpf = $cpf;
+        $this->cidade = $cidade;
+        $this->email = $email;
+        $this->fone = $fone;
     }
+
+
+    /**
+     * @return mixed
+     */
+    public function getNome()
+    {
+        return $this->nome;
+    }
+
+    /**
+     * @param mixed $nome
+     */
+    public function setNome($nome)
+    {
+        $this->nome = $nome;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSobrenome()
+    {
+        return $this->sobrenome;
+    }
+
+    /**
+     * @param mixed $sobrenome
+     */
+    public function setSobrenome($sobrenome)
+    {
+        $this->sobrenome = $sobrenome;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getIdade()
+    {
+        return $this->idade;
+    }
+
+    /**
+     * @param mixed $idade
+     */
+    public function setIdade($idade)
+    {
+        $this->idade = $idade;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCpf()
+    {
+        return $this->cpf;
+    }
+
+    /**
+     * @param mixed $cpf
+     */
+    public function setCpf($cpf)
+    {
+        $this->cpf = $cpf;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCidade()
+    {
+        return $this->cidade;
+    }
+
+    /**
+     * @param mixed $cidade
+     */
+    public function setCidade($cidade)
+    {
+        $this->cidade = $cidade;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param mixed $email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFone()
+    {
+        return $this->fone;
+    }
+
+    /**
+     * @param mixed $fone
+     */
+    public function setFone($fone)
+    {
+        $this->fone = $fone;
+    }
+}
