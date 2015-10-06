05edc13490492cb67c6b6a67f067fd49421c97d7
Alexandre Roch <maycon.alex.rocha@hotmail.com>
2015-10-06 16:47:07 -0300

    criando index.php

---
+++ Class/Cliente.php
@@ -0,0 +1,15 @@
+<?php
+    
+
+    Class Cliente
+    {
+        
+        
+        private $nome;
+        private $sobrenome;
+        private $idade;
+        private $cpf;
+        private $cidade;
+        private $email;
+        private $fone;
+    }
---
+++ index.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <title>Listagem Clientes --A.R.M. Projects--</title>
+        <!-- CSS -->
+        <link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.css">
+        <link rel="stylesheet" href="css/estilo.css">
+    </head>
+    <body>
+        <!-- Conteudo -->   
+        <div class="conteiner-fluid"> 
+            <div class="row">
+                <!-- Header -->
+                <header class="col-md-12 well">
+                    <h1 class="text-center">Clientes A.R.M. Projects</h1>
+                </header>              
+            </div>
+            <div class="row">
+                <!-- Main -->
+                <main class="col-xs-offset-1 col-md-9">
+                    <div class="table-responsive">
+                        <table class="table table-striped table-hover table-bordered">
+                            <thead>
+                                <tr>
+                                    <th>Nome</th>
+                                    <th>Sobrenome</th>
+                                    <th>Idade</th>
+                                    <th>CPF</th>
+                                    <th>Cidade</th>                             
+                                    <th>Email</th>                                 
+                                    <th>Fone</th>                                 
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>x</td>
+                                    <td>x</td>
+                                    <td>x</td>
+                                    <td>x</td>
+                                    <td>x</td>
+                                    <td>x</td>
+                                    <td>x</td>
+                                </tr>
+                                <tr>
+                                    <td>x</td>
+                                    <td>x</td>
+                                    <td>x</td>
+                                    <td>x</td>
+                                    <td>x</td>
+                                    <td>x</td>
+                                    <td>x</td>
+                                </tr>
+                                <tr>
+                                    <td>x</td>
+                                    <td>x</td>
+                                    <td>x</td>
+                                    <td>x</td>
+                                    <td>x</td>
+                                    <td>x</td>
+                                    <td>x</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>                  
+                </main>
+            </div>
+            <div class="row">
+                <!-- Footer -->
+                <footer class="col-md-12"></footer>
+            </div>
+        </div>
+        <!-- JS --> 
+        <script src="bower_components/jquery/dist/jquery.js"></script>
+        <script src="bower_components/bootstrap/dist/js/bootstrap.js"></script>
+    </body>
+</html>
