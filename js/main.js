c48dfefeba864f7bde86f20a2646fe1193f82ae9
Alexandre Roch <maycon.alex.rocha@hotmail.com>
2015-10-09 12:26:16 -0300

    corrigindo main.js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,20 @@
 $(document).ready(function () {
    var tr = $(".trClick");
     tr.click(function () {
-        id = $(this).find("td[data-Id]").data('id');
-        return location.href= location.href+"?cliente="+id;
+        var id = $(this).find("td[data-Id]").data('id'),
+            url = location.href,
+            result = null;
+
+        if(url.indexOf('?') > 0){
+            query = url.split('?');
+
+            result = location.replace("?"+query[1]+"&cliente="+id);
+        }else{
+            result = location.href= location.href+"?cliente="+id;
+        }
+        return result;
     });
+
     tr.hover(function () {
         $(this).css({
             "cursor":"pointer",
